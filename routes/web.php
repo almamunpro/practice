6620d6e34ea1f95101e8a30c96d898e83a1f4db2
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2025-01-10 18:33:05 +0600

    sqlite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home');
@@ -12,47 +15,14 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
 Route::get('/jobs', function () {
     return view('jobs',
-    [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Senior Developer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Project Manager',
-                'salary' => '$8,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Frontend Developer',
-                'salary' => '$7,000'
-            ]
-        ]
-
-    ]);
+['jobs' => job::all()]);
 });
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Senior Developer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Project Manager',
-                'salary' => '$8,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Frontend Developer',
-                'salary' => '$7,000'
-            ]
-        ];
-        $job = Arr::first($jobs, fn($job) => $job['id']== $id);
+
+Route::get('/jobs/{id}', function ($id)  {
+
+        $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
