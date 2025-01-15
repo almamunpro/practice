3ede1667ec25bf3cb80566638c1212a5ac4eba9f
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2025-01-15 23:45:59 +0600

    create fake data

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Job extends Model {
+
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
 }
